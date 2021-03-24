9fedcb15d4b403916475299bad80342934064ed4
kiraxe <kiraxe@yandex.ru>
2021-03-24 12:06:34 +0300

    test

---
+++ src/index.js
@@ -0,0 +1,14 @@
+
+            const $ = require('jquery');
+            const str = require('./constant');
+
+            const App = function() {
+                $(body).html(str);
+            };
+
+            const app = new App();
+            app.get = function() {
+            
+            };
+            console.log();
+        
